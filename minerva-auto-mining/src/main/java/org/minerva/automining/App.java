290e48d4ac783de9640e5f3b547e4ebab2e4d25d
Caio Moreno de Souza <caiomsouza@gmail.com>
2017-06-03 14:04:18 +0200

    Added Minerva Auto Mining

---
+++ minerva-auto-mining/src/main/java/org/minerva/automining/App.java
@@ -0,0 +1,13 @@
+package org.minerva.automining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
